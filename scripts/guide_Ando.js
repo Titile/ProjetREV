0e2047ce593a1b998c57ddc5bb1748fbcc9b0afa
Titile <ando.randrianari@gmail.com>
2016-10-21 14:49:05 +0200

    commit pingouin!!!!

---
+++ scripts/guide_Ando.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", init, false);
+
+function init()
+{
+    alert("Le guide va apparaitre!!!");
+
+    guide = new Guide("", 0.0,0.0,0.0);
+}
+
+class Guide
+{
+    constructor(name, posX, posY, posZ)
+    {
+        if(name=="")
+        {
+            this.name = "Panda"
+        } 
+        else 
+            this.name = name;
+        this.name = name;
+
+        this.position = {"x": posX, "y":posY, "z":posZ};
+        this.scene = document.querySelector('a-scene');
+        //var guide = document.createElement('a-entity');
+        
+        var guide = document.createElement('a-obj-model');     
+
+        guide.setAttribute('id', this.name);
+        guide.setAttribute('position', this.position);     
+       
+        guide.setAttribute('src', "modeles/penguin/penguin.obj");
+        guide.setAttribute('mtl', "modeles/penguin/penguin.mtl" );
+        this.rotation = {"x":-90, "y":-90, "z":-90};
+        guide.setAttribute('rotation', this.rotation);
+
+        this.scene.appendChild(guide);
+    } 
+
+    Execute(acteur)
+    {
+
+    }
+};
+
