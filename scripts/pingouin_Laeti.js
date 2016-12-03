23c557e0099ce5c751f31d02323f33cca0cea658
Laeti <laetitiaonxperia@gmail.com>
2016-12-03 08:41:15 +0100

    Machine à état

--- scripts/pingouin_Laeti.js
+++ scripts/pingouin_Laeti.js
@@ -7,7 +7,6 @@ class Automate
         this.CurrentState = currentState;
         this.CurrentState.enter();
         this.PreviousState = null;
-
     }
 
     // Ce qui va faire évoluer l'état de l'automate 
@@ -21,7 +20,7 @@ class Automate
         this.PreviousState = this.CurrentState;
         this.CurrentState.exit();
         this.CurrentState = NewState;
-        this.CurrentState.enter();
+        this.CurrentState.enter();      
     }
 
     RevertToPreviousState()
@@ -36,37 +35,30 @@ class Guide
     constructor(id)
     {
         this.id = id;
-    }
-
-    Launch()
-    {
+   
         console.log("Instantiation")
 
-         // Instanciation du pingouin
+        // Récupération de la scène
         var scene = document.querySelector('a-scene');
 
-        var panda = document.createElement('a-obj-model');
+        // Placement du guide
+        var guide = document.createElement('a-obj-model');
 
-        panda.setAttribute('id', this.id);
-
-        panda.setAttribute('position', document.getElementById("panda").getAttribute("position"));
-        panda.setAttribute('rotation', this.rotate);
-        panda.setAttribute('scale', this.scale);
-
-        panda.setAttribute('src', "modeles/penguin/penguin.obj");
-        panda.setAttribute('mtl', "modeles/penguin/penguin.mtl");
-        panda.setAttribute('position', "-5 0 0")
-        panda.setAttribute('scale', "2 2 2")
-        panda.setAttribute('rotation', "-90 0 0")
+        guide.setAttribute('id', this.id);   
+        guide.setAttribute('src', "modeles/penguin/penguin.obj");
+        guide.setAttribute('mtl', "modeles/penguin/penguin.mtl");
+        guide.setAttribute('position', "-5 0 0")
+        guide.setAttribute('scale', "2 2 2")
+        guide.setAttribute('rotation', "-90 0 0")
 
         // Gestion du clic
-        panda.addEventListener('click', onGuideClick);
+        guide.addEventListener('click', onGuideClick);
 
-        scene.appendChild(panda);
+        scene.appendChild(guide);
 
         // On le lance
-        var automate = new Automate(this);
-        setInterval(automate.Execute());
+       // var automate = new Automate(this, Waiting);
+        //setInterval(automate.Execute());
     }
 
 
@@ -78,26 +70,26 @@ class Guide
     {
         // Récupération de la caméra
         var camera = document.getElementById("camera");
-        var guide = document.getElementById("panda");
+        var guide = document.getElementById("guide");
 
         var positionCamera = camera.getAttribute('position');
         var positionGuide = guide.getAttribute('position');
 
         // Calcul de la distance dans l'espace
-        var distance = sqrt((positionCamera.x - positionGuide.x)*(positionCamera.x - positionGuide.x) + (positionCamera.y - positionGuide.y)*(positionCamera.y - positionGuide.y) +  (positionCamera.z - positionGuide.z)*(positionCamera.z - positionGuide.z));
-        
+        var distance = Math.sqrt((positionCamera.x - positionGuide.x)*(positionCamera.x - positionGuide.x) + (positionCamera.y - positionGuide.y)*(positionCamera.y - positionGuide.y) +  (positionCamera.z - positionGuide.z)*(positionCamera.z - positionGuide.z));
+
         // Valeur arbitraire
-        if(distance > 1)
+        if(distance > 3.5)
         {
             return true;
         }     
         return false;        
-    }
+}
 
     // Si l'utilisateur est bien avec le guide
     userWithGuide()
     {
-        return !userLost();
+        return !this.userLost();
     }
 
     // Si le pingouin a atteint sa cible  
@@ -105,16 +97,16 @@ class Guide
     {
         // On récupère la target à atteindre
         var target = document.getElementById(targetName);
-        var guide = document.getElementById("panda");
+        var guide = document.getElementById("guide");
 
         var positionTarget = target.getAttribute('position');
         var positionGuide = guide.getAttribute('position');
 
          // Calcul de la distance dans l'espace
-        var distance = sqrt((positionTarget.x - positionGuide.x)*(positionTarget.x - positionGuide.x) + (positionTarget.y - positionGuide.y)*(positionTarget.y - positionGuide.y) +  (positionTarget.z - positionGuide.z)*(positionTarget.z - positionGuide.z));
-        
+        var distance = Math.sqrt((positionTarget.x - positionGuide.x)*(positionTarget.x - positionGuide.x) + (positionTarget.y - positionGuide.y)*(positionTarget.y - positionGuide.y) +  (positionTarget.z - positionGuide.z)*(positionTarget.z - positionGuide.z));
+     
         // Valeur arbitraire
-        if(distance < 1)
+        if(distance <  3.5)
         {
             return true;
         }     
@@ -130,16 +122,12 @@ class Guide
     }    
 };
 
- function onGuideClick(e) 
-        {
-    alert(panda.userReached());
-}
 
-// Initialisation de la scène
-//    constructor(id, sourceObj, sourceMtl,  position, scale, rotation)
-//console.log("njfid");
-
-//var panda = new Guide("panda", "modeles/panda/panda.obj","modeles/panda/panda.mtl", "0 0 0", "2 2 2", "0 0 0");
-var panda = new Guide("panda");
-document.addEventListener("DOMContentLoaded", panda.Launch, false);
+var guide = new Guide("guide");
+document.addEventListener("DOMContentLoaded", guide.Launch, false);
    
+   
+   function onGuideClick(e) 
+        {
+    alert(guide.userLost());
+}
